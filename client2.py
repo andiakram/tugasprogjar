20cca6d8c33da0cfed9a66c605d10f72bd5dccf9
andiakramyusuf <andiakram59@yahoo.co.id>
2015-10-27 16:59:38 +0700

    melengkapi tugas chat client server

--- client2.py
+++ client2.py
@@ -1,41 +1,137 @@
 #!/usr/bin/python
 
+import socket, select, string, sys, time
 
-# telnet program example
-import socket, select, string, sys
- 
-def prompt() :
-    sys.stdout.write('<You> ')
+username=[]
+
+#print you tiap kali
+def prompt():
+    sys.stdout.write("<you> ")
     sys.stdout.flush()
- 
+
+def pisah_string(data):
+    data1=data.split(' ',1)
+    return data1
+
+def print_err(code):
+    if code ==5001:
+        print "CLIENT: ERR 5001 - No message sent, enter your message"
+    elif code==5002:
+        print "CLIENT: ERR 5002 - No message sent, enter recipient username and your message"
+    elif code==5003:
+        print "CLIENT: ERR 5003 - No message sent, invalid recipient"
+    elif code ==9999:
+        print "CLIENT: ERR 9999 - Wrong syntax"
+    elif code==9998:
+        print "SERVER: ERR 9998 - Wrong syntax"
+    elif code==9997:
+        print "CLIENT: ERR 9997 - Wrong recieved data"
+    else:
+        if code ==4001:
+            print "SERVER: ERR 4001 - Can\'t login, username already exist"
+        elif code ==4002:
+            print "SERVER: ERR 4002 - Can\'t login, you are already login"
+        elif code ==4101:
+            print "CLIENT: ERR 4101 - Can\'t login, username and password is same, please change it"
+
+        print "\nSystem will be closed"
+        sys.exit()
+
+def login(sock):
+    print "Enter your username : "
+    usname=sys.stdin.readline()
+    sys.stdin.flush()
+    usname1=pisah_string(usname)
+    print "Enter your password : "
+    passw=sys.stdin.readline()
+    passw1=pisah_string(passw)
+    #print usname1+passw1
+    if usname1==passw1: print_err(4101)
+    else: sock.send("login "+usname1[0]+" "+passw1[0])
+
+def terima_data(sock):
+    #menerima data
+    data=sock.recv(4096)
+    #jika tidak ada data yang diterima
+    if not data:
+        print "\rDisconnected from chat server"
+        sys.exit()
+    else:
+        data1=pisah_string(data)
+        if data1[0]=="2000":
+            #menyimpan username dan status login
+            username=data1[1]
+            sys.stdout.write("{}\rOk, your name is "+data1[1]+"\n")
+            sys.stdout.flush()
+        elif data1[0]=="2001":
+            sys.stdout.write("{}\r"+data1[1])
+            sys.stdout.flush()
+        elif data1[0]=="2002":
+            sys.stdout.write("{}\r"+data1[1])
+            sys.stdout.flush()
+            print "Ok, system will be closed. Goodbye!"
+            sys.exit()
+        elif data1[0]=="4001":
+            print_err(4001)
+        elif data1[0]=="4002":
+            print_err(4002)
+        elif data1[0]=="5003":
+            print_err(5003)
+        else:
+            print_err(9997)
+
+def kirim_data(sock):
+    msg=sys.stdin.readline()
+    msg1=pisah_string(msg)
+    msg2=string.split(msg)
+    #print ukuran
+    ukuran=len(msg2)
+    #print msg1[0][:-1]
+    if msg1[0][:-1]=="sendall" and ukuran<2:
+        print_err(5001)
+    elif msg1[0]=="sendall":
+        sock.send(msg)
+    elif msg1[0][:-1]=="sendto" and ukuran<2:
+        print_err(5002)
+    elif msg1[0]=="sendto" and ukuran<3:
+        print_err(5001)
+    elif msg1[0]=="sendto":
+        sock.send(msg)
+    elif msg1[0][:-1]=="list" and ukuran<2:
+        sock.send(msg)
+    elif msg1[0][:-1]=="logout" and ukuran<2:
+        sock.send(msg)
+    else:
+        print_err(9999)
+
 #main function
 if __name__ == "__main__":
-     
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    # connect to remote host
-    s.connect(("0.0.0.0", 5000))
-    
-    prompt()
+    #coba connect ke server
+    try :
+        s.connect(("0.0.0.0", 5000))
+    except :
+        print "Unable to connect"
+        sys.exit()
      
+    print "Connected to remote host."
+ 
+    login(s)
+ 
+    #prompt()
+ 
     while 1:
-        socket_list = [sys.stdin, s]
-         
-        # Get the list sockets which are readable
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-         
+        socket_list=[sys.stdin, s]
+ 
+        #membaca socket yang ada
+        read_sockets, write_sockets, error_sockets=select.select(socket_list , [], [])
+ 
         for sock in read_sockets:
-            #incoming message from remote server
-            if sock == s:
-                data = sock.recv(4096)
-                
-                #print data
-                sys.stdout.write(data)
+            if sock==s:
+                terima_data(sock)
                 prompt()
-             
-            #user entered a message
-            else :
-                msg = sys.stdin.readline()
-                s.send(msg)
+ 
+            else:
+                kirim_data(s)
                 prompt()
-                
--- server2.py
+++ server2.py
@@ -1,21 +1,125 @@
 #!/usr/bin/python
 
+import socket, select, string
 
-# Tcp Chat server
-import socket, select
- 
-#Function to broadcast chat messages to all connected clients
-def broadcast_data (sock, message):
-    #Do not send the message to master socket and the client who has send us the message
+def pisah_string(data):
+    data1=data.split(' ',1)
+    return data1
+
+def proses_data(sock, data):
+    data1=pisah_string(data)
+    #print data1
+    if data1[0]=='login':
+        log_in(sock, data1[1])
+    elif data1[0]=='sendall':
+        send_all(sock, data1[1])
+    elif data1[0]=='sendto':
+        send_to(sock, data1[1])
+    elif data1[0][:-1]=='list':
+        minta_list(sock)
+    elif data1[0][:-1]=='logout':
+        log_out(sock)
+    else:
+        print "SERVER: ERR 9996 - Wrong recieved data"
+        sock.send(9996)
+
+
+def cek_nama(sock):
+    namaa=[]
+    for x in range (len(NAME_LIST)):
+        if NAME_LIST[x]==sock:
+            namaa=NAME_LIST[x+1]
+            namaa=pisah_string(namaa)
+
+    return namaa[0]
+
+def cek_socket(data):
+    sockeet=[]
+    for x in range (len(NAME_LIST)):
+        tmp=str(NAME_LIST[x])
+        spasi=tmp.count(' ')
+        if spasi>0:
+            namaa=pisah_string(tmp)
+            if namaa[0][:-1]==data:
+                sockeet=NAME_LIST[x-1]
+                continue
+
+    return sockeet
+
+def log_in(sock, data):
+    ceknama=0
+    ceksock=0
+
+    pecah=pisah_string(data)
+    name=pecah[0]
+    password=pecah[1]
+
+    for x in NAME_LIST:
+        xx=pisah_string(str(x))
+        if xx[0]==sock: ceksock=1
+        if xx[0]==name: ceknama=1
+
+    if ceknama==1:
+        print "SERVER: ERR 4001 - Can\'t login, username already exist"
+        sock.send("4001")
+        sock.close()
+    elif ceksock==1:
+        print "SERVER: ERR 4002 - Can\'t login, you are already login"
+        sock.send("4002")
+        sock.close()
+    else:
+        print name[:-1]+" have login with password "+password[:-1]
+        NAME_LIST.append(sock)
+        NAME_LIST.append(name+" "+password)
+        sock.send("2000 "+name[:-1])
+
+def send_all(sock, data):
+    sender=cek_nama(sock)
+    print "Sending message from "+sender[:-1]+" to all"
     for socket in CONNECTION_LIST:
         if socket != server_socket and socket != sock :
-            socket.send(message)
+            socket.send("2001 <sendall from "+sender[:-1]+"> "+data)
+
+def send_to(sock, data):
+    sender=cek_nama(sock)
+    pecah=pisah_string(data)
+    recipient_socket=[]
+    recipient_socket=cek_socket(pecah[0])
+    if not recipient_socket:
+        sock.send("5003")
+        return 0
+    for socket in CONNECTION_LIST:
+        if socket != server_socket and socket == recipient_socket :
+            socket.send("2001 <pm from "+sender[:-1]+"> "+pecah[1])
+            print "Sending message from "+sender[:-1]+" to "+pecah[0]
+            return
+
+def minta_list(sock):
+    sender=cek_nama(sock)
+    print "Sending list to "+sender[:-1]
+    for x in range (len(NAME_LIST)):
+        if x%2==1 and sock!=NAME_LIST[x-1]:
+            namaa=pisah_string(str(NAME_LIST[x]))
+            sock.send("2001 List: "+str(namaa[0]))
+
+def log_out(sock):
+    for x in range (len(NAME_LIST)):
+        if NAME_LIST[x]==sock:
+            namaa=pisah_string(str(NAME_LIST[x+1]))
+            print namaa[0][:-1]+" have logout"
+            sock.send("2002 Ok goodbye "+namaa[0])
+            NAME_LIST.remove[x]
+            NAME_LIST.remove[x+1]
+            NAME_LIST.remove[x+2]
+            sock.close()
+            CONNECTION_LIST.remove(sock)
+            return
 
 if __name__ == "__main__":
      
     # List to keep track of socket descriptors
     CONNECTION_LIST = []
-    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+    NAME_LIST = []
      
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -34,16 +138,16 @@ if __name__ == "__main__":
             if sock == server_socket:
                 sockfd, addr = server_socket.accept()
                 CONNECTION_LIST.append(sockfd)
-                print "Client (%s, %s) connected" % addr
+                print "New client %s, %s" %addr
                  
-                broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
+                #broadcast_data(sockfd, "[%s:%s] entered room\n" % addr)
              
             #Recieve message
             else:
-                try:
-                    data = sock.recv(RECV_BUFFER)
+                try:    
+                    data = sock.recv(4069)
                     if data:
-                        broadcast_data(sock, "\r" + '<' + str(sock.getpeername()) + '> ' + data)                
+                        proses_data(sock, data)
                  
                 except:
                     sock.close()
